fe95f4f8838ffdfe1579d99a5b20efaba6c6814e
Khoa Ho <hokhoa@grinnell.edu>
2017-09-25 15:53:16 -0500

    Add comments

--- Block.java
+++ Block.java
@@ -1,136 +1,148 @@
-import java.nio.ByteBuffer;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Block {
-    private int num;
-    private int amount;
-    private Hash prevHash;
-    private long nonce;
-    private Hash curHash;
+public class BlockChain {
+
+    private class Node {
+        public Block value;
+        public Node next;
+
+        public Node(Block value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int sz;
+    private int anna;
+    private int bob;
 
     /**
-     * Constructs a new block with given parameters
+     * Initializes a blockchain that possess a single block the starts with the
+     * given initial amount
      * 
-     * @param num
-     *            the number of the block in the blockchain
-     * @param amount
-     *            the amount transferred from one account to another (from Anna to
-     *            Bob)
-     * @param prevHash
-     *            the hash from the previous block in the chain
+     * @param initial
+     *            the initial amount that Alice has
      * @throws NoSuchAlgorithmException
      */
-    public Block(int num, int amount, Hash prevHash) throws NoSuchAlgorithmException {
-        this.num = num;
-        this.amount = amount;
-        this.prevHash = prevHash;
-        this.nonce = 0;
-        calculateHash();
-        while (!curHash.isValid()) {
-            nonce++;
-            calculateHash();
+    public BlockChain(int initial) throws NoSuchAlgorithmException {
+        this.sz = 0;
+        this.anna = initial;
+        this.bob = 0;
+        this.first = new Node(new Block(sz++, initial, null), null);
+        this.last = this.first;
+    }
+
+    /**
+     * Mines a new candidate block to be added to the list
+     * 
+     * @param amount
+     *            the amount transferred
+     * @return the candidate block
+     * @throws NoSuchAlgorithmException
+     */
+    public Block mine(int amount) throws NoSuchAlgorithmException {
+        return new Block(sz, amount, last.value.getHash());
+    }
+
+    /**
+     * Gets the size of the blockchain
+     * 
+     * @return the size of the blockchain
+     */
+    public int getSize() {
+        return sz;
+    }
+
+    /**
+     * Appends this block to the end of the chain
+     * 
+     * @param blk
+     *            the block to be added
+     * @throws IllegalArgumentException
+     *             if this block cannot be added to the list (because it is invalid
+     *             wrt the rest of the blocks)
+     */
+    public void append(Block blk) {
+        if (blk.getHash().isValid()) {
+            Node n = new Node(blk, null);
+            last.next = n;
+            last = n;
+            sz++;
+            anna += blk.getAmount();
+            bob -= blk.getAmount();
+        } else {
+            System.out.println("The given block has invalid hash!");
+            throw new IllegalArgumentException();
         }
     }
 
     /**
-     * Constructs a new block with given parameters
+     * Removes the last block from the chain
      * 
-     * @param num
-     *            the number of the block in the blockchain
-     * @param amount
-     *            the amount transferred from one account to another (from Anna to
-     *            Bob)
-     * @param prevHash
-     *            the hash from the previous block in the chain
-     * @param nonce
-     *            the nonce of the block
-     * @throws NoSuchAlgorithmException
+     * @return true if a block is removed
      */
-    public Block(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
-        this.num = num;
-        this.amount = amount;
-        this.prevHash = prevHash;
-        this.nonce = nonce;
-        calculateHash();
-    }
+    public boolean removeLast() {
+        if (sz == 0) {
+            return false;
+        } else {
+            Block lastBlk = last.value;
+            anna -= lastBlk.getAmount();
+            bob += lastBlk.getAmount();
 
-    /**
-     * Gets the number of the block
-     * 
-     * @return the number of the block
-     */
-    public int getNum() {
-        return num;
-    }
-
-    /**
-     * Gets amount transferred in the block
-     * 
-     * @return the amount transferred in the block
-     */
-    public int getAmount() {
-        return amount;
-    }
-
-    /**
-     * Calculates the hash value for the block
-     * 
-     * @throws NoSuchAlgorithmException
-     */
-    public void calculateHash() throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("sha-256");
-
-        byte[] numByteBuffer = ByteBuffer.allocate(4).putInt(num).array();
-        byte[] amountByteBuffer = ByteBuffer.allocate(4).putInt(amount).array();
-        byte[] nonceByteBuffer = ByteBuffer.allocate(8).putLong(nonce).array();
-
-        md.update(numByteBuffer);
-        md.update(amountByteBuffer);
-        if (num != 0) {
-            md.update(prevHash.getData());
+            Node cur = first;
+            while (cur.next.next != null) {
+                cur = cur.next;
+            }
+            cur.next = null;
+            last = cur;
+            sz--;
+            return true;
         }
-        md.update(nonceByteBuffer);
-
-        curHash = new Hash(md.digest());
     }
 
     /**
-     * Gets the nonce in the block
+     * Gets the hash of the last block in the chain
      * 
-     * @return the nonce of the block
-     */
-    public long getNonce() {
-        return nonce;
-    }
-
-    /**
-     * Gets the previous hash value of the previous block
-     * 
-     * @return the hash of the previous block
-     */
-    public Hash getPrevHash() {
-        return prevHash;
-    }
-
-    /**
-     * Gets the hash from the current block
-     * 
-     * @return the hash of this block
+     * @return the hash
      */
     public Hash getHash() {
-        return curHash;
+        return last.value.getHash();
     }
 
     /**
-     * Formats block information into a string representation
+     * Walks the blockchain and ensures that its blocks are consistent and valid
      * 
-     * @return a string representation of the block in the specified format
+     * @return true if the balance of each person at the end of the chain is
+     *         non-negative
+     */
+    public boolean isValidBlockChain() {
+        return bob >= 0 && anna >= 0;
+    }
+
+    /**
+     * Prints the balance of each person at the end of the chain
+     */
+    public void printBalance() {
+        System.out.println(
+                "Alice: " + Integer.toString(anna) + ", " + "Bob: " + Integer.toString(bob));
+    }
+
+    /**
+     * Represents the blockchain as a string representation
+     * 
+     * @return the string representation of each of its blocks, earliest to latest,
+     *         one per line
      */
     public String toString() {
-        return "Block " + Integer.toString(num) + " (Amount: " + Integer.toString(amount) + ", "
-                + "Nonce: " + Long.toString(nonce) + ", " + "prevHash: "
-                + ((prevHash != null) ? prevHash.toString() : "null") + ", " + "hash: "
-                + curHash.toString() + ")";
+        Node cur = first;
+        StringBuilder builder = new StringBuilder();
+        while (cur != null) {
+            builder.append("\n");
+            builder.append(cur.value.toString());
+            cur = cur.next;
+        }
+        return builder.toString();
     }
 }
