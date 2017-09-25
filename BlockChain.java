2dc4267af12b6750bd90773c35dbc38cf510e2d6
Khoa Ho <hokhoa@grinnell.edu>
2017-09-25 15:54:25 -0500

    Add comments

--- BlockChain.java
+++ BlockChain.java
@@ -18,6 +18,14 @@ public class BlockChain {
     private int anna;
     private int bob;
 
+    /**
+     * Initializes a blockchain that possess a single block the starts with the
+     * given initial amount
+     * 
+     * @param initial
+     *            the initial amount that Alice has
+     * @throws NoSuchAlgorithmException
+     */
     public BlockChain(int initial) throws NoSuchAlgorithmException {
         this.sz = 0;
         this.anna = initial;
@@ -26,14 +34,36 @@ public class BlockChain {
         this.last = this.first;
     }
 
+    /**
+     * Mines a new candidate block to be added to the list
+     * 
+     * @param amount
+     *            the amount transferred
+     * @return the candidate block
+     * @throws NoSuchAlgorithmException
+     */
     public Block mine(int amount) throws NoSuchAlgorithmException {
         return new Block(sz, amount, last.value.getHash());
     }
 
+    /**
+     * Gets the size of the blockchain
+     * 
+     * @return the size of the blockchain
+     */
     public int getSize() {
         return sz;
     }
 
+    /**
+     * Appends this block to the end of the chain
+     * 
+     * @param blk
+     *            the block to be added
+     * @throws IllegalArgumentException
+     *             if this block cannot be added to the list (because it is invalid
+     *             wrt the rest of the blocks)
+     */
     public void append(Block blk) {
         if (blk.getHash().isValid()) {
             Node n = new Node(blk, null);
@@ -43,10 +73,16 @@ public class BlockChain {
             anna += blk.getAmount();
             bob -= blk.getAmount();
         } else {
+            System.out.println("The given block has invalid hash!");
             throw new IllegalArgumentException();
         }
     }
 
+    /**
+     * Removes the last block from the chain
+     * 
+     * @return true if a block is removed
+     */
     public boolean removeLast() {
         if (sz == 0) {
             return false;
@@ -54,7 +90,7 @@ public class BlockChain {
             Block lastBlk = last.value;
             anna -= lastBlk.getAmount();
             bob += lastBlk.getAmount();
-            
+
             Node cur = first;
             while (cur.next.next != null) {
                 cur = cur.next;
@@ -66,18 +102,39 @@ public class BlockChain {
         }
     }
 
+    /**
+     * Gets the hash of the last block in the chain
+     * 
+     * @return the hash
+     */
     public Hash getHash() {
         return last.value.getHash();
     }
 
+    /**
+     * Walks the blockchain and ensures that its blocks are consistent and valid
+     * 
+     * @return true if the balance of each person at the end of the chain is
+     *         non-negative
+     */
     public boolean isValidBlockChain() {
-         return bob >= 0 && anna >= 0;
+        return bob >= 0 && anna >= 0;
     }
 
+    /**
+     * Prints the balance of each person at the end of the chain
+     */
     public void printBalance() {
-        System.out.println("Alice: " + Integer.toString(anna) + ", " + "Bob: " + Integer.toString(bob));
+        System.out.println(
+                "Alice: " + Integer.toString(anna) + ", " + "Bob: " + Integer.toString(bob));
     }
 
+    /**
+     * Represents the blockchain as a string representation
+     * 
+     * @return the string representation of each of its blocks, earliest to latest,
+     *         one per line
+     */
     public String toString() {
         Node cur = first;
         StringBuilder builder = new StringBuilder();
@@ -88,5 +145,4 @@ public class BlockChain {
         }
         return builder.toString();
     }
-
 }
