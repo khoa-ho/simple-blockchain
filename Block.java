09c13162637a37292868b29f0f86f605d78f5c6b
Khoa Ho <hokhoa@grinnell.edu>
2017-09-25 15:52:37 -0500

    Add comments

--- Block.java
+++ Block.java
@@ -9,6 +9,18 @@ public class Block {
     private long nonce;
     private Hash curHash;
 
+    /**
+     * Constructs a new block with given parameters
+     * 
+     * @param num
+     *            the number of the block in the blockchain
+     * @param amount
+     *            the amount transferred from one account to another (from Anna to
+     *            Bob)
+     * @param prevHash
+     *            the hash from the previous block in the chain
+     * @throws NoSuchAlgorithmException
+     */
     public Block(int num, int amount, Hash prevHash) throws NoSuchAlgorithmException {
         this.num = num;
         this.amount = amount;
@@ -21,6 +33,20 @@ public class Block {
         }
     }
 
+    /**
+     * Constructs a new block with given parameters
+     * 
+     * @param num
+     *            the number of the block in the blockchain
+     * @param amount
+     *            the amount transferred from one account to another (from Anna to
+     *            Bob)
+     * @param prevHash
+     *            the hash from the previous block in the chain
+     * @param nonce
+     *            the nonce of the block
+     * @throws NoSuchAlgorithmException
+     */
     public Block(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
         this.num = num;
         this.amount = amount;
@@ -29,14 +55,29 @@ public class Block {
         calculateHash();
     }
 
+    /**
+     * Gets the number of the block
+     * 
+     * @return the number of the block
+     */
     public int getNum() {
         return num;
     }
 
+    /**
+     * Gets amount transferred in the block
+     * 
+     * @return the amount transferred in the block
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Calculates the hash value for the block
+     * 
+     * @throws NoSuchAlgorithmException
+     */
     public void calculateHash() throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("sha-256");
 
@@ -54,18 +95,38 @@ public class Block {
         curHash = new Hash(md.digest());
     }
 
-    public long getNonce() throws NoSuchAlgorithmException {
+    /**
+     * Gets the nonce in the block
+     * 
+     * @return the nonce of the block
+     */
+    public long getNonce() {
         return nonce;
     }
 
+    /**
+     * Gets the previous hash value of the previous block
+     * 
+     * @return the hash of the previous block
+     */
     public Hash getPrevHash() {
         return prevHash;
     }
 
+    /**
+     * Gets the hash from the current block
+     * 
+     * @return the hash of this block
+     */
     public Hash getHash() {
         return curHash;
     }
 
+    /**
+     * Formats block information into a string representation
+     * 
+     * @return a string representation of the block in the specified format
+     */
     public String toString() {
         return "Block " + Integer.toString(num) + " (Amount: " + Integer.toString(amount) + ", "
                 + "Nonce: " + Long.toString(nonce) + ", " + "prevHash: "
